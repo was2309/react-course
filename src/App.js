64fc632df25a4851f66a036af1af332d8a54183f
was2309 <vasilijem92@gmail.com>
2022-11-04 12:31:02 +0100

    Create expense item and expense date component. Use props.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const expensies = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ]
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expensies[0].title} amount={expensies[0].amount} date={expensies[0].date}/>
+            <ExpenseItem title={expensies[1].title} amount={expensies[1].amount} date={expensies[1].date}/>
+            <ExpenseItem title={expensies[2].title} amount={expensies[2].amount} date={expensies[2].date}/>
+            <ExpenseItem title={expensies[3].title} amount={expensies[3].amount} date={expensies[3].date}/>
+        </div>
+    );
 }
 
 export default App;
