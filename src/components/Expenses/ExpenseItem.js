60c63b774ce5179ea79191c545a2b7bd6642ac45
was2309 <vasilijem92@gmail.com>
2022-11-07 13:03:53 +0100

    Use state. Add expense form and expense filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,10 @@
+import React from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
+import React, {useState} from "react";
+
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from './ExpenseFilter';
 import Card from "../UI/Card";
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangedHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
         <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
