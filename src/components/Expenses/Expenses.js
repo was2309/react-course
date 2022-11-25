0adda8d8ddf5b1fec26c117ad865ce0f2d774a1c
was2309 <vasilijem92@gmail.com>
2022-11-25 18:58:39 +0100

    Add filter for year (conditional rendering)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from './ExpenseFilter';
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -12,18 +12,22 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+
+
     return (
-        <Card className="expenses">
-            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-            {props.items.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangedHandler}
                 />
-            ))}
-        </Card>
+                <ExpensesList items={filteredExpenses}/>
+            </Card>
+        </div>
     );
 }
 
